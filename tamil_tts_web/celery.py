525ace76c1fa4cbb442f354a048b608ea07ce523
fossbalaji <fossbalaji@gmail.com>
2018-11-30 05:14:31 +0530

    celery rename try1

--- tamil_tts_web/celery.py
+++ tamil_tts_web/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tamil_tts_web.settings')
 
-app = Celery('tamiltts')
+app = Celery('tamil_tts_web')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
